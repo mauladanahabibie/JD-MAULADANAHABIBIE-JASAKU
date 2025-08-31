15b58326bf25b08b32e67ab52ed99de1f97feb99
mauladanahabibie <mauladanah@gmail.com>
2025-08-31 18:39:57 +0700

    update favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JasaKu - Marketplace Jasa</title>
     @vite('resources/css/app.css')
+    <link rel="shortcut icon" href="{{ asset('assets/logo.png') }}" type="image/x-icon">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
 </head>
 
--- resources/views/layanan.blade.php
+++ resources/views/layanan.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JasaKu - Semua Layanan</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="{{ asset('assets/logo.png') }}" type="image/x-icon">
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
 </head>
