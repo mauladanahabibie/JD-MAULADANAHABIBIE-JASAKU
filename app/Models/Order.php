cc1fbb3e2f63130b90dc2beb15718a18dc9f40f0
mauladanahabibie <mauladanah@gmail.com>
2025-09-16 13:35:03 +0700

    update fix error accept order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,15 +8,37 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
-
+    
+    const STATUS_BELUM_BAYAR = 'belum_bayar';
+    const STATUS_DIBAYAR     = 'dibayar';
+    const STATUS_DIPROSES    = 'diproses';
+    const STATUS_SELESAI     = 'selesai';
+    const STATUS_DIBATALKAN  = 'dibatalkan';
+    
     protected $table = 'orders';
-
+    
     protected $fillable = [
         'service_id',
         'customer_id',
         'mitra_id',
         'description',
+        'price',
         'status',
         'bukti',
     ];
+    
+    public function service()
+    {
+        return $this->belongsTo(Service::class);
+    }
+    
+    public function customer()
+    {
+        return $this->belongsTo(User::class, 'customer_id');
+    }
+    
+    public function mitra()
+    {
+        return $this->belongsTo(User::class, 'mitra_id');
+    }
 }
